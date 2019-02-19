87fce5a0f8d3b12bad95aca9feb2045d587641e8
Murtaza Raja <murtraja@gmail.com>
2019-02-19 07:56:01 +0530

    adding files necessary for heroku

--- main.py
+++ main.py
@@ -23,4 +23,7 @@ def route_test():
     db_helper.test()
     return "OK"
 
-run(host='0.0.0.0', port=8080, debug=True)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='0.0.0.0', port=8080, debug=True)
